2b3ecd99b7fba8254f2177c2fde679dfd24a8d07
Askerko Alexandr <askerko50@gmail.com>
2022-07-24 22:11:58 +0300

    add junior junior+ fix route

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import {NavLink} from "react-router-dom";
+import s from './Header.module.css'
 
 function Header() {
     return (
-        <div>
-            // add NavLinks
-
+        <div className={s.NavBody}>
+            <button className={s.NavBotton}> menu</button>
+            <div className={s.NavContent}>
+                <div className={s.NavItem}><NavLink to="/pre-junior">PreJunior</NavLink>
+                </div>
+                <div className={s.NavItem}><NavLink to="/junior">Junior</NavLink>
+                </div>
+                <div className={s.NavItem}><NavLink to="/junior+">Junior+</NavLink>
+                </div>
+            </div>
         </div>
     )
 }
--- src/p2-homeworks/h5-rrd-v6/pages/Error404.tsx
+++ src/p2-homeworks/h5-rrd-v6/pages/Error404.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
-
+import s from './Error404.module.css'
+import {NavLink} from "react-router-dom";
 function Error404() {
     return (
-        <div>
-            <div>404</div>
-            <div>Page not found!</div>
-            <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
+        <div className={s.MainDiv}>
+
+            <h1>Error: 404 page not found</h1>
+            <p>Sorry, the page you're looking for cannot be accessed</p>
+
+            <div><NavLink to="/" className={s.navlink}>Back to MainPage</NavLink></div>
+
+
+
         </div>
     )
 }
