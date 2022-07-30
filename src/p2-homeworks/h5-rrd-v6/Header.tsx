a07783a630fdca7d54005b74e77d27b4e3a0727d
Askerko Alexandr <askerko50@gmail.com>
2022-07-30 20:03:41 +0300

    fix header

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -7,11 +7,13 @@ function Header() {
         <div className={s.NavBody}>
             <button className={s.NavBotton}> menu</button>
             <div className={s.NavContent}>
-                <div className={s.NavItem}><NavLink to="/pre-junior">PreJunior</NavLink>
-                </div>
-                <div className={s.NavItem}><NavLink to="/junior">Junior</NavLink>
-                </div>
-                <div className={s.NavItem}><NavLink to="/junior+">Junior+</NavLink>
+                <div className={s.NavContentWrapper}>
+                    <div className={s.NavItem}><NavLink to="/pre-junior">PreJunior</NavLink>
+                    </div>
+                    <div className={s.NavItem}><NavLink to="/junior">Junior</NavLink>
+                    </div>
+                    <div className={s.NavItem}><NavLink to="/junior+">Junior+</NavLink>
+                    </div>
                 </div>
             </div>
         </div>
